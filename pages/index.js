81b15b1a0509264db340091240fcf2a7a4d1e165
arushi2527 <arushiyadavolf@gmail.com>
2025-05-11 20:21:54 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
 import { Button, CircularProgress, Container, Dialog, Typography } from '@mui/material';
 import { auth } from '../firebase/firebase';
@@ -29,11 +30,16 @@ const REDIRECT_PAGE = '/dashboard';
 const uiConfig = {
   signInFlow: 'popup', // signIn flow with popup rather than redirect flow
   signInSuccessUrl: REDIRECT_PAGE,
+  signInOptions: [
+    // can be Google, Facebook, Twitter, Github, email/password, etc
+    EmailAuthProvider.PROVIDER_ID,
+    GoogleAuthProvider.PROVIDER_ID, 
+    ]
 }
 
 export default function Home() {
-  const router = useRouter();
-
+  const [login, setLogin] = useState(false);
+  
   return (
     <div>
       <Head>
@@ -45,10 +51,14 @@ export default function Home() {
           <Typography variant="h1">Welcome to Expense Tracker!</Typography>
           <Typography variant="h2">Add, view, edit, and delete expenses</Typography>
           <div className={styles.buttons}>
-            <Button variant="contained" color="secondary">
+            <Button variant="contained" color="secondary"
+              onClick={() => setLogin(true)}>
               Login / Register
             </Button>
           </div>
+          <Dialog open={login} onClose={() => setLogin(false)} >
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth}></StyledFirebaseAuth>
+          </Dialog>
         </Container>
       </main>
     </div>);
