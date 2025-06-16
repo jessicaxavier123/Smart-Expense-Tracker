e3fc963244d31bd0f405bbcfae0863981d02d1b3
jessicaxavier123 <jx200526@gmail.com>
2025-06-16 13:52:03 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
-import '../styles/global.scss'
+import '../styles/global.scss';
 import '../styles/firebaseui-styling.global.scss';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../styles/theme.js';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AuthUserProvider } from '../src/firebase/auth';
 
 export default function App({ Component, pageProps }) {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </LocalizationProvider>);
+  <AuthUserProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </LocalizationProvider>
+    </AuthUserProvider>
+  );
 }
