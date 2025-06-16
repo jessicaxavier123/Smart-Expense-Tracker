c96f1200a26bbe7e3c2ea35c57a8af8ca5d1621c
jessicaxavier123 <jx200526@gmail.com>
2025-06-16 13:43:57 -0700

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,45 +1,37 @@
-/**
- * @license
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { AppBar, Box, Button, Container, Stack, Toolbar, Typography } from '@mui/material';
-import { useAuth } from '../firebase/auth';
+import { useAuth } from '../src/firebase/auth';
 import styles from '../styles/navbar.module.scss';
+import { useRouter } from 'next/router';
+
 
 export default function NavBar() {
+const { authUser, signOut } = useAuth();
+const router = useRouter();
 
-  return (
+const handleLogout = async () => {
+    await signOut();
+    router.replace('/');    // send them back to your login page
+};
+
+return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" className={styles.appbar}>
+    <AppBar position="static" className={styles.appbar}>
         <Toolbar className={styles.toolbar}>
-          <Container className={styles.container}>
+        <Container className={styles.container}>
             <Typography variant="h3" sx={{ flexGrow: 1, alignSelf: "center" }}>
-              EXPENSE TRACKER
+            EXPENSE TRACKER
             </Typography>
             <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
-              <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                Insert user email here
-              </Typography>
-              <Button variant="text" color="secondary">
-                Logout
-              </Button>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                {authUser?.email}
+            </Typography>
+            <Button variant="text" color="secondary" onClick = {handleLogout}>
+                LOGOUT
+            </Button>
             </Stack>
-          </Container>
+        </Container>
         </Toolbar>
-      </AppBar>
+    </AppBar>
     </Box>
-  );
+);
 }
